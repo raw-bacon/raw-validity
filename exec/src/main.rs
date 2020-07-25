adc109734ae7ef1fd297891afde4e2b5c062eef5
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-25 21:27:57 +0200

    fixed dummy executable

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use std::io;
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+    let mut buffer = String::new();    
+    io::stdin().read_line(&mut buffer)?;
+    let s = String::from("hello computer\n");
+    if buffer == s {
+        println!("Oh, that's cute, :)");
+    }
+
+    return Ok(());
 }
