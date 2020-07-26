6ae83749746a8774544aa2c99d91f0dacbbb2f49
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-26 12:29:30 +0200

    finished parsing of l-group terms

--- parsing/src/lib.rs
+++ parsing/src/lib.rs
@@ -1,11 +1,3 @@
 pub mod parse_literal;
 pub mod parse_free_group_term;
 pub mod parse_l_group_term;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- parsing/src/parse_l_group_term.rs
+++ parsing/src/parse_l_group_term.rs
@@ -15,7 +15,7 @@ use std::collections::BTreeSet;
 /// use terms::literal::lit;
 /// use terms::Term;
 /// let term = LGroupTerm::from(lit('x').inverse());
-/// assert_eq!(Ok(term), parse_l_group_term::parse(String::from("-x")))
+/// assert_eq!(Ok(term), parse_l_group_term::parse(&String::from("-x")))
 /// ```
 /// Multiplication of terms bigger than literals is also by writing them next to
 /// each other:
@@ -28,13 +28,14 @@ use std::collections::BTreeSet;
 /// meetands.insert(LGroupTerm::from(lit('y')));
 /// meetands.insert(LGroupTerm::from(lit('z')));
 /// let term = LGroupTerm::from(lit('x')) *  LGroupTerm::Meet(meetands);
-/// assert_eq!(Ok(term), parse_l_group_term::parse(String::from("x(yvz)")));
+/// assert_eq!(Ok(term), parse_l_group_term::parse(&String::from("x(y^z)")));
 /// ```
-pub fn parse(s: String) -> Result<LGroupTerm, String> {
-    let mut s = s;
-    // remove whitespace>
+pub fn parse(s: &String) -> Result<LGroupTerm, String> {
+    println!("{}", &s);
+    let mut s = s.clone();
+    // remove whitespace and outer brackets
     s.retain(|c| !c.is_whitespace());
-    while only_has_outermost_brackets(&s) {
+    while s.len() >= 2 && only_has_outermost_brackets(&s) {
         s = s[1..s.len() - 1].to_string();
     }
 
@@ -47,7 +48,7 @@ pub fn parse(s: String) -> Result<LGroupTerm, String> {
     }
     
     if is_inverse(&s) {
-        let result = parse(s[1..s.len()].to_string());
+        let result = parse(&s[1..s.len()].to_string());
         match result {
             Ok(term) => return Ok(term.inverse()),
             Err(e) => return Err(format!("Parsing this inverse failed: {}, {}", &s, e))
@@ -58,7 +59,7 @@ pub fn parse(s: String) -> Result<LGroupTerm, String> {
         let mut meetands = BTreeSet::new();
 
         for term_string in s.split("^") {
-            let result = parse(term_string.to_string());
+            let result = parse(&term_string.to_string());
             match result {
                 Ok(term) => meetands.insert(term),
                 Err(e) => return Err(format!("Parsing this meet failed: {}, {}", &s, e))
@@ -71,7 +72,7 @@ pub fn parse(s: String) -> Result<LGroupTerm, String> {
         let mut joinands = BTreeSet::new();
 
         for term_string in s.split("v") {
-            let result = parse(term_string.to_string());
+            let result = parse(&term_string.to_string());
             match result {
                 Ok(term) => joinands.insert(term),
                 Err(e) => return Err(format!("Parsing this join failed: {}, {}", &s, e))
@@ -80,7 +81,60 @@ pub fn parse(s: String) -> Result<LGroupTerm, String> {
         return Ok(LGroupTerm::Join(joinands));
     }
 
-    todo!()
+    let mut factors = Vec::new();
+    let mut current_factor = String::new();
+    let mut depth = 0;
+    for c in s.chars() {
+        match &c {
+            '(' => {
+                if depth == 0 && current_factor.len() > 0 {
+                    let result = parse(&current_factor);
+                    match result {
+                        Ok(term) => factors.push(term),
+                        Err(e) => return Err(format!("Parsing the product {} at {} failed. {}", &s, &current_factor, e))
+                    };
+                    current_factor = String::new();
+                }
+                depth += 1;
+                current_factor.push(c);
+            },
+            ')' => {
+                match depth {
+                    0 => return Err(String::from("Brackets do not match!")),
+                    _ => depth -= 1
+                };
+                current_factor.push(c);
+                if depth == 0 {
+                    let result = parse(&current_factor);
+                    match result {
+                        Ok(term) => factors.push(term),
+                        Err(e) => return Err(format!("Parsing the product {} at {} failed. {}", &s, &current_factor, e))
+                    };
+                    current_factor = String::new();
+                }
+            },
+            '-' => {
+                if depth == 0 && current_factor.len() > 0 {
+                    let result = parse(&current_factor);
+                    match result {
+                        Ok(term) => factors.push(term),
+                        Err(e) => return Err(format!("Parsing the product {} at {} failed. {}", &s, &current_factor, e))
+                    };
+                    current_factor = String::new();
+                }
+                current_factor.push(c);
+            }
+            _ => current_factor.push(c)
+        };
+    }
+    if current_factor.len() > 0 {
+        let result = parse(&current_factor);
+            match result {
+                Ok(term) => factors.push(term),
+                Err(e) => return Err(format!("Parsing the product {} at {} failed. {}", &s, &current_factor, e))
+            };
+    }
+    return Ok(LGroupTerm::Prod(factors));
 }
 
 /// Returns whether the input contains `^`, `v`, or `-`
@@ -93,23 +147,54 @@ fn is_atom(s: &String) -> bool {
 
 /// Warning: only save to call if it is known not to be an atom
 fn is_inverse(s: &String) -> bool {
-    todo!()
+    let mut chars = s.chars();
+    let c = chars.next();
+    if c != Some('-') { return false };
+    let mut depth = 0;
+    for c in chars {
+        match &c {
+            '(' => depth += 1,
+            ')' => depth -= 1,
+            'v' | '^' | '-'  => { if depth == 0 { return false; }},
+            _ => {}
+        };
+    }
+    return true;
 }
 
 /// Warning: only save to call if it is known not to be an atom or an inverse
 fn is_meet(s: &String) -> bool {
-    todo!()
+    let mut depth = 0;
+    for c in s.chars() {
+        match &c {
+            '(' => depth += 1,
+            ')' => depth -= 1,
+            _ => {}
+        };
+        if depth == 0 && c == '^' { return true };
+    }
+    return false;
 }
 
 /// Warning: only save to call if it is known not to be an atom or an inverse
 fn is_join(s: &String) -> bool {
-    todo!()
+    let mut depth = 0;
+    for c in s.chars() {
+        match &c {
+            '(' => depth += 1,
+            ')' => depth -= 1,
+            _ => {}
+        };
+        if depth == 0 && c == 'v' { return true };
+    }
+    return false;
 }
 
 
 /// returns true if it is of the form `(...(x)...)`, where
 /// `x` does not contain any brackets. 
 fn only_has_outermost_brackets(s: &String) -> bool {
+    #[derive(PartialEq, Eq)]
     enum Position { Left, Middle, Right };
     let mut pos = Position::Left;
     for c in s.chars() {
@@ -126,5 +211,5 @@ fn only_has_outermost_brackets(s: &String) -> bool {
             }
         };
     }
-    return true;
+    return pos == Position::Right;
 }
