f73344765a2a798b11fd61bb3243ea1714ef6f8a
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-27 04:23:44 +0200

    refactored literals

--- terms/src/literal.rs
+++ terms/src/literal.rs
@@ -1,4 +1,4 @@
-use super::*;
+use super::Term;
 
 /// The smallest term (apart from the identity).
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -66,6 +66,16 @@ impl ToString for Literal {
     }
 }
 
+impl From<&str> for Literal {
+    fn from(s: &str) -> Literal {
+        let result = parse(s);
+        match result {
+            Ok(literal) => literal,
+            Err(e) => panic!(e)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -76,5 +86,77 @@ mod tests {
         assert_eq!("X", lit ('x').inverse().to_string());
         let l = Literal::new('x', 31, true);
         assert_eq!("X31", l.to_string());
+    }
+}
+
+
+/// parses a literal from a string. 
+/// 
+/// # Examples
+/// Basic usage:
+/// Symbols with ids are okay.
+/// ```
+/// use parsing::parse_literal;
+/// use terms::literal::Literal;
+/// let string = String::from("X31");
+/// let literal = Literal::new('x', 31, true);
+/// assert_eq!(Ok(literal), parse_literal::parse(string));
+/// ```
+/// So are symbols without.
+/// ```
+/// # use parsing::parse_literal;
+/// # use terms::literal::Literal;
+/// let string = String::from("y");
+/// let literal = Literal::new('y', 0, false);
+/// assert_eq!(Ok(literal), parse_literal::parse(string));
+/// ```
+fn parse(s: &str) -> Result<Literal, ParsingError> {
+    let l = s.len();
+    if l == 0 {
+        return Err(ParsingError::EmptyLiteralError);
+    } else {
+        let is_inverted: bool;
+        let mut lower_case: std::char::ToLowercase;
+        match s.chars().next() {
+            None => return Err(ParsingError::EmptyLiteralError),
+            Some(c) => { is_inverted = c.is_uppercase(); lower_case = c.to_lowercase(); }
+        };
+        let character: char;
+        match lower_case.next() {
+            None => return Err(ParsingError::EmptyLiteralError),
+            Some(c) => character = c
+        };
+        if l == 1 { return Ok(Literal::new(character, 0, is_inverted)); }
+
+        let id = without_first(s).parse::<usize>().expect(
+            "could not parse ID. a literal should be a character, possibly followed
+            by an id (a non-negative integer)"
+        );
+        return Ok(Literal::new(character, id, is_inverted))
+    }
+}
+
+fn without_first(string: &str) -> String {
+    let mut result = String::new();
+    let mut iterator = string.chars();
+    iterator.next();
+    for c in iterator { result.push(c); }
+
+    return result;
+}
+
+pub enum ParsingError {
+    EmptyLiteralError,
+    NoLowerCaseError(char)
+}
+
+impl std::fmt::Display for ParsingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            ParsingError::EmptyLiteralError => 
+                write!(f, "Empty literal could not be parsed."),
+            ParsingError::NoLowerCaseError(c) => 
+                write!(f, format!("The character {} does not have a lower case.", c))
+        }
     }
 }
