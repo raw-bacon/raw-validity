bd7344cfac269856c2c96b9915784629821efdb8
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-29 11:31:34 +0200

    another try

---
+++ docs/pkg/docs.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function main(): void;
+/**
+* @param {string} s
+* @returns {boolean}
+*/
+export function is_valid(s: string): boolean;
---
+++ docs/pkg/docs.js
@@ -0,0 +1,3 @@
+import * as wasm from "./docs_bg.wasm";
+export * from "./docs_bg.js";
+wasm.__wbindgen_start();
